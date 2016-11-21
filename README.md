f7a7ba88b7f9c322800705873476f50d37b76276
Wayne Lu <wayne.lu.8@gmail.com>
2016-11-20 21:56:47 -0500

    More features

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ CS 229 Final Project by Kyle Julian and Wayne Lu. Attempting to apply a machine 
 
 # Dependencies
 * [SNAP](http://snap.stanford.edu/snappy/index.html#download)
+* scikit-learn
+* numpy
 
 # Datasets
 * [SNAP](http://snap.stanford.edu/data/index.html) again
--- featureextraction.py
+++ featureextraction.py
@@ -1,16 +1,24 @@
 import snap
 import sys
 
-SRC_D_IN = 0
-SRC_D_OUT = 1
-DST_D_IN = 2
-DST_D_OUT = 3
-NEIGHBORS = 4
-TRIAD_0 = 5
-TRIAD_1 = 6
-TRIAD_2 = 7
-TRIAD_3 = 8
-NUM_FEATURES = 9
+NUM_FEATURES = 17
+SRC_D_IN, \
+SRC_D_OUT, \
+SRC_IN_OUT, \
+SRC_OUT_IN, \
+DST_D_IN, \
+DST_D_OUT, \
+DST_IN_OUT, \
+DST_OUT_IN, \
+NEIGHBORS, \
+TRIAD_0, \
+TRIAD_1, \
+TRIAD_2, \
+TRIAD_3, \
+TRIAD_0_N, \
+TRIAD_1_N, \
+TRIAD_2_N, \
+TRIAD_3_N = range(NUM_FEATURES)
 
 def extractFeatures(graph, srcNodeID, dstNodeID):
     """
@@ -20,13 +28,21 @@ def extractFeatures(graph, srcNodeID, dstNodeID):
     Features:
         SRC_D_IN : in-degree of src
         SRC_D_OUT : out-degree of src
+        SRC_IN_OUT : in-degree / out-degree of src
+        SRC_OUT_IN : out-degree / in-degree of src
         DST_D_IN : in-degree of dst
         DST_D_OUT : out-degree of dst
+        DST_IN_OUT : in-degree / out-degree of dst
+        DST_OUT_IN : out-degree / in-degree of dst
         NEIGHBORS : number of nodes adjacent to both src and dst
         TRIAD_0 : participation in triads of form src -> neighbor -> dst
         TRIAD_1 : participation in triads of form src -> neighbor <- dst
         TRIAD_2 : participation in triads of form src <- neighbor -> dst
         TRIAD_3 : participation in triads of form src <- neighbor <- dst
+        TRIAD_0_N : TRIAD_0 / NEIGHBORS ratio
+        TRIAD_1_N : TRIAD_1 / NEIGHBORS ratio
+        TRIAD_2_N : TRIAD_2 / NEIGHBORS ratio
+        TRIAD_3_N : TRIAD_3 / NEIGHBORS ratio
 
     Args:
         graph (snap.TNGraph or snap.TUNGraph): the graph to extract features from
@@ -45,8 +61,12 @@ def extractFeatures(graph, srcNodeID, dstNodeID):
     # Basic feature extraction
     features[SRC_D_IN] = srcNode.GetInDeg()
     features[SRC_D_OUT] = srcNode.GetOutDeg()
+    features[SRC_IN_OUT] = features[SRC_D_IN] * 1.0 / features[SRC_D_OUT]
+    features[SRC_OUT_IN] = features[SRC_D_OUT] * 1.0 / features[SRC_D_IN]
     features[DST_D_IN] = dstNode.GetInDeg()
     features[DST_D_OUT] = dstNode.GetOutDeg()
+    features[DST_IN_OUT] = features[DST_D_IN] * 1.0 / features[DST_D_OUT]
+    features[DST_OUT_IN] = features[DST_D_OUT] * 1.0 / features[DST_D_IN]
 
     # Path lengths. Set to sys.maxint if no path exists
     #srcDstPath = snap.GetShortPath(graph, srcNodeID, dstNodeID, True)
@@ -72,5 +92,10 @@ def extractFeatures(graph, srcNodeID, dstNodeID):
     features[TRIAD_1] = len(srcOutNeighbors & dstOutNeighbors)
     features[TRIAD_2] = len(srcInNeighbors & dstInNeighbors)
     features[TRIAD_3] = len(srcInNeighbors & dstOutNeighbors)
+
+    features[TRIAD_0_N] = features[TRIAD_0] * 1.0 / features[NEIGHBORS]
+    features[TRIAD_1_N] = features[TRIAD_1] * 1.0 / features[NEIGHBORS]
+    features[TRIAD_2_N] = features[TRIAD_2] * 1.0 / features[NEIGHBORS]
+    features[TRIAD_3_N] = features[TRIAD_3] * 1.0 / features[NEIGHBORS]
 
     return features
